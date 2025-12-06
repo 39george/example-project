6d0c4005230015091756f91b6e2992f79736b1f4
39george <obsidian.musicwork@gmail.com>
2025-12-06 19:20:22 +0300

    fix: remove unused require

--- src/app.lua
+++ src/app.lua
@@ -1,17 +1,14 @@
 local lapis = require("lapis")
-local redis = require("redis")
 local db = require("lapis.db")
 
 local app = lapis.Application()
 
 app:get("/", function(self)
-  local count = redis.incr("page_views") or 1
   local res = db.query("select * from my_table where id = ?", 10)
 
   return {
     json = {
       message = "Welcome",
-      page_views = count,
       timestamp = os.date("%Y-%m-%d %H:%M:%S"),
     },
   }
